b27284aa42b0fcf61baa3e49237b9ffe300c3211
rxrosario1117 <99367990+rxrosario1117@users.noreply.github.com>
2022-11-27 00:42:17 -0500

    Update to folder structure

---
+++ src/Components/DisplayComponents/calcDisplay.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import "../../App.css"
+import "./calcDisplay.css"
+const CalcDisplay = () => {
+    return (
+    <div>
+        Hello
+    </div>
+
+    );
+}
+
+export default CalcDisplay;
